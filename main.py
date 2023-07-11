451eaf916e1ec9080b444ab5af864270aff8436f
Roey-Shap <roeyshapiro15@gmail.com>
2023-07-11 16:16:18 +0300

    Did first successful full spells page creation run

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from PIL import Image
 import csv
 
 baseFilePath = "C:\\Users\\Roey Shapiro\\Documents\\AAB Backup\\College\\Year 3\\Set\\DeckOfDecks\\"
-cardData: dict[int, int] = {}
+cardData: dict[list[int], int] = {}
 outOfBoundsValue = 1000
 pageDims = (2000, 2828)
 cardDimsOnPage = (623, 869)
+zeroCopyCardsNames = []
 
 def createPages():
     x = 0
@@ -37,9 +38,6 @@ def createPages():
         except:
             print(f"An image for card #{cardFilePath} couldn't be found.")
 
-
-
-
     if cardsOnPage < 9:
         SavePage(fullPage, currentPageIndex)
 
@@ -49,14 +47,18 @@ def GetSpreadSheetData():
     with open(baseFilePath + "Formatter\\MTGCard\\DOD.csv") as file:
         csvReader = csv.DictReader(file)
         for row in csvReader:
-            rowIndex = ParseNullableInt(row["INDEX"])
+            rowIndex = ParseNullableVal(row["INDEX"])
+            numCopies = ParseNullableVal(row["#"])
+            if (numCopies <= 0 or numCopies >= outOfBoundsValue) and (len(row["NAME"]) > 0):
+                zeroCopyCardsNames.append(row["NAME"])
             if 0 < rowIndex < outOfBoundsValue:
-                cardData[rowIndex] = ParseNullableInt(row["#"])
+                cardData[rowIndex] = numCopies if (0 < numCopies < outOfBoundsValue) else 0
 
 
-def ParseNullableInt(string):
+def ParseNullableVal(string):
     if len(string) == 0:
         return outOfBoundsValue
+
     return int(string)
 
 def SavePage(page, currentPageIndex):
@@ -67,7 +69,7 @@ if __name__ == '__main__':
     print("Running...")
     print("Populating card data from spreadsheet...")
     GetSpreadSheetData()
-    # cardData = {1: 7, 2: 4, 3: 3}
     print("Creating page images...")
     numPages, totalCards = createPages()
     print(f"... Done! Created {numPages} pages with {totalCards} out of a total of {sum(cardData.values())} total cards on them.")
+    print(f"\nNote that {zeroCopyCardsNames[0:-1]}, and {zeroCopyCardsNames[-1]} were marked as 'don't print' in the spreadsheet.")
